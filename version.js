23846e4b39d5a09d500de7d365eac7b246c19ee8
winton <mail@wintoni.us>
2019-03-22 00:18:03 -0400

    Moving glob logic out of cli

--- version.js
+++ version.js
@@ -1,95 +1,43 @@
-import fs from "fs-extra"
-import { join } from "path"
-import semver from "semver"
-
 module.exports = function(dot) {
   if (dot.version) {
     return
   }
 
   dot("dependencies", "version", {
-    arg: ["@dot-event/store", "@dot-event/wait"],
+    arg: [
+      "@dot-event/args",
+      "@dot-event/glob",
+      "@dot-event/store",
+      "@dot-event/wait",
+    ],
   })
+
+  dot("args", "version", {
+    paths: {
+      alias: ["_", "p"],
+      default: [],
+    },
+  })
+
+  require("./versionProject")(dot)
 
   dot.any("version", version)
 }
 
 async function version(prop, arg, dot) {
-  const { paths } = arg,
-    baseProp = prop.slice(0, -1)
-
-  let { path } = arg
-
-  if (!path.match(/\/package\.json$/)) {
-    path = join(path, "package.json")
-  }
-
-  var pkg
-
-  if (await fs.pathExists(path)) {
-    pkg = await fs.readJson(path)
-  }
-
-  if (pkg) {
-    const {
-      dependencies,
-      devDependencies,
-      name,
-      version,
-    } = pkg
-
-    await Promise.all([
-      setVersions(dot, { [name]: version }),
-      setVersions(dot, dependencies),
-      setVersions(dot, devDependencies),
-    ])
-  }
-
-  await dot.wait(baseProp, "gatherVersions", {
-    count: paths.length,
+  const paths = await dot.glob(prop, {
+    absolute: true,
+    pattern: "{" + arg.paths.join(",") + "}",
   })
 
-  if (pkg) {
-    const { dependencies, devDependencies } = pkg
-
-    for (const name in dependencies) {
-      dependencies[name] = dot.get("versions", name)
-    }
-
-    for (const name in devDependencies) {
-      devDependencies[name] = dot.get("versions", name)
-    }
-
-    await fs.writeJson(path, pkg, { spaces: 2 })
-  }
-}
-
-async function setVersions(dot, deps = {}) {
   return Promise.all(
-    Object.keys(deps).map(name => {
-      return dot.set("versions", name, current => {
-        let version = fixDep(deps[name])
-
-        if (current) {
-          version = semver.gt(current, version)
-            ? current
-            : version
-        }
-
-        return version
-      })
-    })
+    paths.map(
+      async path =>
+        await dot.versionProject(prop, {
+          ...arg,
+          path,
+          paths,
+        })
+    )
   )
-}
-
-function fixDep(dep) {
-  return dep
-    ? semver.coerce(dep).version + depHyphen(dep)
-    : "0.0.0"
-}
-
-function depHyphen(dep) {
-  return dep && dep.indexOf("-") > -1
-    ? "-" + dep.split(/-/)[1]
-    : ""
 }
---
+++ versionProject.js
@@ -0,0 +1,91 @@
+import fs from "fs-extra"
+import { join } from "path"
+import semver from "semver"
+
+module.exports = function(dot) {
+  if (dot.versionProject) {
+    return
+  }
+
+  dot.any("versionProject", versionProject)
+}
+
+async function versionProject(prop, arg, dot) {
+  const { paths } = arg,
+    baseProp = prop.slice(0, -1)
+
+  let { path } = arg
+
+  if (!path.match(/\/package\.json$/)) {
+    path = join(path, "package.json")
+  }
+
+  var pkg
+
+  if (await fs.pathExists(path)) {
+    pkg = await fs.readJson(path)
+  }
+
+  if (pkg) {
+    const {
+      dependencies,
+      devDependencies,
+      name,
+      version,
+    } = pkg
+
+    await Promise.all([
+      setVersions(dot, { [name]: version }),
+      setVersions(dot, dependencies),
+      setVersions(dot, devDependencies),
+    ])
+  }
+
+  await dot.wait(baseProp, "gatherVersions", {
+    count: paths.length,
+  })
+
+  if (pkg) {
+    const { dependencies, devDependencies } = pkg
+
+    for (const name in dependencies) {
+      dependencies[name] = dot.get("versions", name)
+    }
+
+    for (const name in devDependencies) {
+      devDependencies[name] = dot.get("versions", name)
+    }
+
+    await fs.writeJson(path, pkg, { spaces: 2 })
+  }
+}
+
+async function setVersions(dot, deps = {}) {
+  return Promise.all(
+    Object.keys(deps).map(name => {
+      return dot.set("versions", name, current => {
+        let version = fixDep(deps[name])
+
+        if (current) {
+          version = semver.gt(current, version)
+            ? current
+            : version
+        }
+
+        return version
+      })
+    })
+  )
+}
+
+function fixDep(dep) {
+  return dep
+    ? semver.coerce(dep).version + depHyphen(dep)
+    : "0.0.0"
+}
+
+function depHyphen(dep) {
+  return dep && dep.indexOf("-") > -1
+    ? "-" + dep.split(/-/)[1]
+    : ""
+}
